dfb71b3423bdc8beb5c963ad28bb2e9e78243ddd
099Al <efimov_as@tochka.com>
2025-06-12 16:40:24 +0500

    scroll color

--- main.py
+++ main.py
@@ -18,6 +18,22 @@ def main(page: ft.Page):
         curr_view = d_routes[page.route]
         page.views.append(curr_view.view(page, None, None))
 
+        page.theme = ft.Theme(
+            scrollbar_theme=ft.ScrollbarTheme(
+                #track_visibility=True,
+                track_border_color=ft.Colors.BLUE,
+                #thumb_visibility=True,
+                thumb_color={
+                    ft.ControlState.HOVERED: ft.Colors.GREY_300,
+                    ft.ControlState.DEFAULT: ft.Colors.GREY_800,
+                },
+                #thickness=30,
+                #radius=15,
+                #main_axis_margin=5,
+                #cross_axis_margin=10,
+            )
+        )
+
         page.update()
 
     def view_pop(view):
