750527f33270b8629ad9dbf8bf62044798165f40
099Al <efimov_as@tochka.com>
2025-06-22 14:33:07 +0500

    refactor client  generic header

--- pages/dashboard/content/clients/clients.py
+++ pages/dashboard/content/clients/clients.py
@@ -4,8 +4,9 @@ from database.models.models import ClientsBan
 from database.requests.req_clients import ReqClients
 from pages.config.errors import d_error_messages_admin
 from pages.config.sizes import d_client_column_size
-from pages.dashboard.content.clients.clients_elements import ClientRow, ClientHeader
+from pages.dashboard.content.clients.clients_elements import ClientRow
 from pages.dashboard.content.filter_header import GenericFilter
+from pages.dashboard.content.header import GenericHeader
 from pages.dashboard.head_elements import header
 
 
@@ -19,13 +20,24 @@ class ClientsContent:
         self.view_content = []
         self.error_messages = d_error_messages_admin
 
-        self.field_definitions = [
+        self.field_definitions_filter = [
             ("name", True),
             ("phone", True),
             # ("some_static_field", "c_static", False),  # not filterable, space reserved
             ("email", True),
             ("telegram_name", True),
             ("telegram_link", True),
+        ]
+
+        self.field_definitions_header = [
+            {"label": "", "field_name": "edit", "sort_attr": None, "is_sortable": False, "type": None},
+            {"label": "Имя", "field_name": "name", "sort_attr": "name", "is_sortable": True, "type": str},
+            {"label": "Телефон", "field_name": "phone", "sort_attr": None, "is_sortable": False, "type": None},
+            {"label": "Email", "field_name": "email", "sort_attr": "email", "is_sortable": True, "type": str},
+            {"label": "Telegram", "field_name": "telegram_name", "sort_attr": "telegram_name", "is_sortable": True, "type": str},
+            {"label": "Telegram Link", "field_name": "telegram_link", "sort_attr": "telegram_link", "is_sortable": True, "type": str},
+            {"label": "Бан", "field_name": "is_banned", "sort_attr": "is_banned", "is_sortable": True, "type": int},
+            {"label": "Причина", "field_name": "ban_reason", "sort_attr": None, "is_sortable": False, "type": None}
         ]
 
     def build(self):
@@ -47,8 +59,16 @@ class ClientsContent:
 
         self.column_1 = ft.Column(
             controls=[
-                GenericFilter(self.page, self.column_with_rows.controls, self.field_definitions, d_client_column_size).build(),
-                ClientHeader(self.page, self.column_with_rows.controls).build(),
+                GenericFilter(self.page, self.column_with_rows.controls, self.field_definitions_filter, d_client_column_size).build(),
+                GenericHeader(
+                    self.page,
+                    self.column_with_rows.controls,
+                    self.field_definitions_header,
+                    d_client_column_size,
+                    default_sort_key='telegram_id',
+                    sort_key_type=int,
+                    sort_key_reverse=True,
+                ).build(),
                 self.column_with_rows
             ],
             expand=True  # без expand scroll не работает
