a995bbd48bc110bfce45f4c8906a46f2ecd2ce7e
099Al <efimov_as@tochka.com>
2025-06-22 20:56:51 +0500

    choose_menu  orders

---
+++ pages/dashboard/content/orders/orders.py
@@ -0,0 +1,13 @@
+import flet as ft
+
+
+
+class OrdersContent:
+    def __init__(self, instance):
+        self.page = instance.page
+        self.user_role = instance.user_role
+        self.view_content = []
+        # self.error_messages = d_error_messages_admin
+
+    def build(self):
+        pass
